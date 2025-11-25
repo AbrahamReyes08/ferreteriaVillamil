d5698aba361a8c16a3a80716fac38b14f3318237
AbrahamReyes08 <124213092+AbrahamReyes08@users.noreply.github.com>
2025-11-24 22:20:39 -0600

    controller para eliminar una calificacion

--- backend/routes/calificacion.js
+++ backend/routes/calificacion.js
@@ -3,5 +3,6 @@ const calificacionCtrl = require("../controllers/calificacion");
 
 router.get("/list", calificacionCtrl.getAllCalificaciones);
 router.post("/create", calificacionCtrl.createNewCalificacion);
+router.delete("/delete/:id", calificacionCtrl.deleteCalificacion);
 
 module.exports = router;
